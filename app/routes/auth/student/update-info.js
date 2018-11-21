1391f89466f262ae80d34c191d058e3dd6fad05a
AlexElITIC <A01271558@itesm.mx>
2018-11-20 21:16:22 -0600

    terminado

--- app/routes/auth/student/update-info.js
+++ app/routes/auth/student/update-info.js
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
-
+import RSVP from 'rsvp';
 export default Route.extend({
-  model(params) {
-return this.store.findRecord('student', params.id)
-}
+  model(params){
+
+      return RSVP.hash({
+        reservation: this.store.query('reservation', {student_id: params.id}),
+        student: this.store.findRecord('student', params.id)
+      })
+    }
 });
